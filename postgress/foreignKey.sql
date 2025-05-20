0bb6557992acee6c9fa8652054e528b5109df45f
Mahdi Hassan <mahdioffi333@gmail.com>
2025-05-20 15:31:29 +0600

    Constructing Relationships with Foreign Key Constraints

---
+++ postgress/foreignKey.sql
@@ -0,0 +1,10 @@
+CREATE TABLE "user"(
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(25) NOT NULL
+)
+
+CREATE Table post(
+    id SERIAL PRIMARY KEY,
+    title TEXT NOT NULL,
+    user_id INTEGER REFERENCES "user"(id)
+)
