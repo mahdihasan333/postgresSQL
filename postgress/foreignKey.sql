236d2967e59cb7706014577f3b308cc12d3bc7dd
Mahdi Hassan <mahdioffi333@gmail.com>
2025-05-20 17:31:32 +0600

    Enforcing Referential Integrity: Behaviors During Data Insertion

--- postgress/foreignKey.sql
+++ postgress/foreignKey.sql
@@ -7,4 +7,44 @@ CREATE Table post(
     id SERIAL PRIMARY KEY,
     title TEXT NOT NULL,
     user_id INTEGER REFERENCES "user"(id)
-)
+);
+
+
+ALTER TABLE post
+    alter COLUMN user_id set NOT NULL
+
+
+INSERT INTO "user" (username) VALUES
+('akash'),
+('batash'),
+('sagor'),
+('nodi');
+
+INSERT INTO post(title, user_id) VALUES
+('Enjoying a sunny day with Akash!', 2),
+('Batash just shared an amazing recipe!', 1),
+('Exploring adventures with sagor!', 4),
+('Nodi`s wisdom always leaves me inspired!', 4);
+
+
+SELECT * FROM post;
+
+
+INSERT INTO post(title, user_id) VALUES('test', 8);
+INSERT INTO post(title, user_id) VALUES('test', 1);
+
+INSERT INTO post(title, user_id) VALUES('test', NULL);
+
+-- Insertion constraint on INSERT post
+-- Attempting to insert a post with a user ID that does not exist
+-- Inserting a post with a valid user ID
+-- Attempting to insert a post without specifying a user ID
+
+
+
+
+-- Deletion constraint on DELETE user
+-- Restrict Deletion -> ON DELETE RESTRICT / ON DELETE NO ACTION (default)
+-- Cascading Deletion -> ON DELETE CASCADE
+-- Setting NULL -> ON DELETE SET NULL
+-- Set Default value -> ON DELETE SET DEFAULT
