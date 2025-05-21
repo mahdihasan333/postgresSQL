bfc39482684d44cb7202e41ba8a1bd5590a118b9
Mahdi Hassan <mahdioffi333@gmail.com>
2025-05-21 16:01:18 +0600

    Query Practice: 2

--- postgress/practice.sql
+++ postgress/practice.sql
@@ -74,3 +74,53 @@ JOIN departments USING(department_id)
 GROUP BY department_name;
 
 
+SELECT department_name, count(employee_id) FROM employees
+JOIN departments USING(department_id)
+GROUP BY department_name;
+
+
+SELECT department_name, round(avg(salary)) as avg_salary FROM employees
+JOIN departments USING(department_id)
+GROUP BY department_name
+ORDER BY avg_salary desc
+LIMIT 1
+;
+
+SELECT extract(YEAR from hire_date) as hire_year, count(*) from employees
+GROUP BY hire_year;
+
+
+
+
+
+
+CREATE TABLE orders (
+    order_id SERIAL PRIMARY KEY,
+    customer_id INT,
+    order_date DATE,
+    total_amount DECIMAL(10, 2)
+);
+
+INSERT INTO orders (customer_id, order_date, total_amount) VALUES 
+    (1, '2022-01-05', 100.50),
+    (2, '2020-01-07', 200.75),
+    (1, '2022-01-08', 150.25),
+    (3, '2020-05-10', 300.00),
+    (2, '2022-01-15', 180.50),
+    (3, '2022-01-20', 220.25),
+    (1, '2022-01-25', 90.00),
+    (2, '2022-01-28', 120.75),
+    (3, '2021-02-01', 250.50),
+    (1, '2022-02-05', 180.25),
+    (4, '2023-02-05', 190.25);
+
+DROP Table orders;
+
+-- Find customers who have placed more than 2 orders and calculate the total amount spent by each of these customers.
+
+
+
+SELECT customer_id, count(order_id), sum(total_amount) as total_spent from orders GROUP BY customer_id HAVING count(order_id) > 2;
+
+    -- Find the total amount of orders placed each month in the year 2022.
+SELECT extract(month from order_date) as month, sum(total_amount)  from orders WHERE extract(year from order_date) = 2022 GROUP BY month;
