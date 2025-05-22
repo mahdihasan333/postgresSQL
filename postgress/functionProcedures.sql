9c62874b51475970c71955fe33e1709117a3157a
Mahdi Hassan <mahdioffi333@gmail.com>
2025-05-23 00:12:57 +0600

    Exploring Stored Procedure In PostgreSQL

--- postgress/functionProcedures.sql
+++ postgress/functionProcedures.sql
@@ -60,5 +60,48 @@ $$
     DELETE FROM employees WHERE employee_id = p_emp_id;
 $$;
 
-SELECT delete_emp_by_id(29)
+SELECT delete_emp_by_id(29);
 
+
+
+
+CREATE Procedure remove_emp()
+LANGUAGE plpgsql
+AS
+$$
+    BEGIN
+        DELETE FROM employees WHERE employee_id = 28
+    END
+$$;
+
+CALL remove_emp();
+
+CREATE Procedure remove_emp_var()
+LANGUAGE plpgsql
+AS
+$$
+    DECLARE
+    test_var int;
+    BEGIN
+        SELECT employee_id INTO test_var from employees WHERE employee_id = 26;
+        DELETE FROM employees WHERE employee_id = test_var;
+    END
+$$;
+
+call remove_emp_var();
+
+CREATE Procedure remove_emp_by_id(p_emp_id int)
+LANGUAGE plpgsql
+AS
+$$
+    DECLARE
+    test_var int;
+    BEGIN
+        SELECT employee_id INTO test_var from employees WHERE employee_id = p_emp_id;
+        DELETE FROM employees WHERE employee_id = test_var;
+        RAISE NOTICE 'Employee removed successfully!';
+    END
+$$;
+
+
+CALL remove_emp_by_id(25)
